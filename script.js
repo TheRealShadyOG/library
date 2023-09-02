f250a873ccbc3ca001ce3ff2d01b5c1742582122
Shady <116603365+TheRealShadyOG@users.noreply.github.com>
2023-09-02 14:42:57 -0500

    Add two books and add to myLibrary

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ function Book(name, pages, read) {
 function addBookToLibrary(object) {
     myLibrary.push(object);
 }
+
+const quran = new Book("Quran", "604", "Read");
+addBookToLibrary(quran);
+
+const purificationOfTheHeart = new Book("Purification Of The Heart by Hamza Yusuf", "220", 
+                                        "Currently Reading");
+addBookToLibrary(purificationOfTheHeart);
